d851d9f55e485fe2595342104aad763a1178a921
alliajagbe <alliajagbe001@gmail.com>
2023-07-18 17:50:14 +0530

    word cloud function

--- functions.py
+++ functions.py
@@ -2,6 +2,8 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.probability import FreqDist
 import string
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
 def header_df(df):
     df = df.iloc[2:]
@@ -25,3 +27,12 @@ def frequency_analysis(df, col):
     stripped = [w.translate(table) for w in tokens]
     stripped = [w for w in stripped if w.isalpha()]
     return stripped
+
+def word_cloud(text, list_of_words):
+    freq_dist = FreqDist(list_of_words)
+    wordcloud = WordCloud(width=800, height=400, background_color="white").generate_from_frequencies(freq_dist)
+    plt.figure(figsize=(20, 10))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+    plt.savefig(text + '.png')
+    plt.show()
