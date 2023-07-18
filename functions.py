b1e931501a45cadc90be81e3174e6fb2ed86acc5
alliajagbe <alliajagbe001@gmail.com>
2023-07-18 17:14:47 +0530

    freq analysis function

--- functions.py
+++ functions.py
@@ -1,3 +1,7 @@
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.probability import FreqDist
+
 def header_df(df):
     df = df.iloc[2:]
     new_header = df.iloc[0]
@@ -6,3 +10,15 @@ def header_df(df):
 
     df = df.reset_index(drop=True)
     return df
+
+
+def freq_analysis(df, col):
+    text = ''.join(df[col])
+    text = text.lower()
+    tokens = word_tokenize(text)
+    stop_words = set(stopwords.words('english'))
+    tokens = [w for w in tokens if not w in stop_words]
+    freq_dist = FreqDist(tokens)
+    for word, frequency in freq.most_common(10):
+        print(u'{};{}'.format(word, frequency))
+    return freq_dist
